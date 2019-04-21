cbe6755fdbf1efde23e5db23c22846331cfa1e72
ShvarcbergJull <32794912+ShvarcbergJull@users.noreply.github.com>
2019-04-22 00:14:59 +0800

    Create form.php

---
+++ form.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Thank you</title>
+</head>
+<body> 
+	<h2><font color="blue">Ваша заявка на участие отправлена успешно!</font></h2> 
+	<p><a href="/SHU">Отправить заявку ещё раз!</a></p> 
+	
+</body> 
+</html>
+
